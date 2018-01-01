0a5e4dda938e8936f9510dcfb5ffd38bf35faf57
Turreta.com <Turreta@users.noreply.github.com>
2018-01-01 16:06:51 +0800

    Add files via upload

---
+++ src/main/java/com/turreta/bhmp/comturretabuildhelpermavenpluginaddresourcedemo/ComTurretaBuildHelperMavenPluginAddResourceDemoApplication.java
@@ -0,0 +1,12 @@
+package com.turreta.bhmp.comturretabuildhelpermavenpluginaddresourcedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComTurretaBuildHelperMavenPluginAddResourceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComTurretaBuildHelperMavenPluginAddResourceDemoApplication.class, args);
+	}
+}
